7c4614f8feb06f359a9245af65a25068040b0de2
celinaninadr <dricicelina@icloud.com>
2025-04-06 22:41:39 +0200

    [ADD]changement nom bdd

--- common/config.php
+++ common/config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=biblioweb", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=biblio_db", "root", "");
 
     //Configuration de PDO pour permettre la bonne gestion des erreurs
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
