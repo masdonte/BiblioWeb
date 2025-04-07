012036773ceae0848d4c60587d7532984247d120
celinaninadr <dricicelina@icloud.com>
2025-04-07 22:13:18 +0200

    [ADD]barre de recherche

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=biblio_db", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur : " . $e->getMessage());
+}
+
+
+
+// Requête SQL si une recherche a été effectuée
+if (!empty($recherche)) {
+    $stmt = $pdo->prepare("SELECT * FROM livres WHERE titre ");
+    $articles = $stmt;
+} else {
+    // Pour éviter les erreurs si aucune recherche n'est faite
+    $articles = $pdo->query("SELECT * FROM livres LIMIT 0");
+}
+
 // Récupération de la route depuis l'URL
 $page = isset($_GET['page']) ? $_GET['page'] : 'login';
 
 // Définition des pages autorisées
 $pages = ['login', 'signon', 'livredetail', 'home'];
 
-
 if ($page !== 'login') {
     include 'common/navbar.php';
 }
-
 
 // Vérification et inclusion de la bonne page
 if (in_array($page, $pages)) {
@@ -20,17 +36,20 @@ if (in_array($page, $pages)) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recherche</title>
 </head>
-
-
 <body>
 
-</body>
+    <form method="GET">
+        <input type="search" name="recherche" placeholder="Recherche..." />
+        <input type="submit" value="Valider" />
+    </form>
+   
+   
 
+</body>
 </html>
