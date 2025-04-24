b64d1c1af77e8eeee56b46e93ace9292c5c3930f
unknown <reda.oukhira@SIO.local>
2025-04-24 13:41:55 +0200

    add permission file

---
+++ common/permission.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+# https://code.whatever.social/questions/10541439/header-location-relative-path-compatibility#20097052
+
+
+
+define('URL', 'http://localhost/BiblioWeb/');
+
+// Vérifiez si l'utilisateur est connecté
+if (!isset($_SESSION['connected']) || !$_SESSION['connected']) {
+    echo "<script>alert('Connectez-vous !');</script>";
+    header('Location: ' . URL . 'index.php?pages=login.php');
+    exit();
+}
+
+if ($_SESSION['role'] !== 'admin') {
+    echo "<script>alert('Vous n'y avez pas accès');</script>";
+    header('Location: ' . URL . 'index.php?pages=home.php');
+
+    exit();
+}
+
+?>
