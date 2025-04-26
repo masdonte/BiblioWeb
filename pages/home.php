19882770a3e80c1c3fbba2123819e9230d3189ad
masdonte <136394162+masdonte@users.noreply.github.com>
2025-04-26 18:24:19 +0200

    fix session_start

--- pages/home.php
+++ pages/home.php
@@ -10,9 +10,12 @@
     <h1>Bienvenue sur Biblioweb.</h1>
     <h2>Le lieu où trouver vos livres.</h2>
 
-    <?php if !==(!isset($_SESSION['connected']) || !$_SESSION['connected']) {
-   echo ' <h2>Vous ne savez pas par où commencer ?</h2>'
-    }?>
+    <?php
+    session_start();
+    if (!isset($_SESSION['connected']) || !$_SESSION['connected']) {
+        echo '<h2>Vous ne savez pas par où commencer ?</h2>';
+    }
+    ?>
     ;
 
 </body>
