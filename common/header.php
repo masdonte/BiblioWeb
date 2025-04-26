944b48d2e9d035840665aec3754b6cb5147a54c7
masdonte <136394162+masdonte@users.noreply.github.com>
2025-04-26 20:09:13 +0200

    fix home.php

--- common/header.php
+++ common/header.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION)) {
         <?php else: ?>
             <a href="pages/livres.php">Livres</a> |
             <a href="pages/emprunts.php">Mes Emprunts</a> |
-            <?php if ($_SESSION['user']['role'] == 'admin'): ?>
+            <?php if ($_SESSION['is_admin'] == true): ?>
                 <a href="/BiblioWeb/pages/admin/admin_dashboard.php">Admin</a> |
             <?php endif; ?>
             <a href="/BiblioWeb/index.php?page=logout">Déconnexion</a>
