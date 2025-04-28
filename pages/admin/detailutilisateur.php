7d01b4505dc6eb4d599ce1b230b1bfab7c4c3abd
celinaninadr <dricicelina@icloud.com>
2025-04-28 13:54:16 +0200

    sous forme de tableau pour meilleur organisation

--- pages/admin/detailutilisateur.php
+++ pages/admin/detailutilisateur.php
@@ -23,18 +23,31 @@ $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+<table border="1" cellpadding="10">
+        <thead>
+            <tr>
+            <th>ID de l'utilisateur </th>
+                <th>Nom de l'utilisateur</th>
+                <th>Adresse mail de l'utilisateur</th>
+                <th>Livre emprunté </th>
+            </tr>
+        </thead>
 <?php
+
     foreach($utilisateurs as $utilisateur) { ?>
-        <p>Nom de l'utilisateur : <?php echo htmlspecialchars($utilisateur["nom"]); ?></p>
-        <p>Adresse mail de l'utilisateur : <?php echo htmlspecialchars($utilisateur["email"]); ?></p>
+       <td><?= htmlspecialchars(string: $utilisateur['nom']) ?></td>
+       <td><?= htmlspecialchars($utilisateur['email']) ?></td>
     <?php } ?>
 
     <?php
     foreach($livres as $livre) { ?>
-        <p>Livre emprunté : <?php echo htmlspecialchars($livre["titre"]); ?></p>
-        <p>ID : <?php echo htmlspecialchars($livre["id"]); ?></p>
+         <td><?= htmlspecialchars($livre['id']) ?></td>
+                   
+                   <td><?= htmlspecialchars($livre['titre']) ?></td>
     <?php } ?>
     
+   
+    
+
 </body>
 </html>
