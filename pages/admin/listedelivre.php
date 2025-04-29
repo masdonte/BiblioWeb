9ca9bf5abf2bbd6585f528be95db473105b964eb
masdonte <136394162+masdonte@users.noreply.github.com>
2025-04-29 20:26:13 +0200

    ajout style pour

--- pages/admin/listedelivre.php
+++ pages/admin/listedelivre.php
@@ -14,28 +14,30 @@ $stmt->execute();
 $livre = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
 </head>
+
 <body>
-    
-<?php
-    foreach($livre as $livr) {?>
-    <p> Nom du livre : <?php echo $livr ["titre"]; ?> </p>
-    <p> Auteur : <?php echo $livr ["auteur"]; ?> </p>
-    <p> genre : <?php echo $livr ["genre"]; ?> </p>
-    <p> statut : <?php echo $livr ["statut"]; ?> </p>
-   
+
     <?php
+    foreach ($livre as $livr) { ?>
+        <p> Nom du livre : <?php echo $livr["titre"]; ?> </p>
+        <p> Auteur : <?php echo $livr["auteur"]; ?> </p>
+        <p> genre : <?php echo $livr["genre"]; ?> </p>
+        <p> statut : <?php echo $livr["statut"]; ?> </p>
+
+        <?php
     }
     ?>
 
-       
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.jade.min.css"
->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.jade.min.css">
+    <link rel="stylesheet" href="../../public/style.css">
+
 </body>
+
 </html>
