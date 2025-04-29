8a9a025b5da6661f61920a29228890120a7f4921
masdonte <136394162+masdonte@users.noreply.github.com>
2025-04-29 20:19:54 +0200

    card pour afficher la liste utilisateur

--- pages/admin/listeutilisateur.php
+++ pages/admin/listeutilisateur.php
@@ -14,25 +14,31 @@ $stmt->execute();
 $utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<?php
-    foreach($utilisateurs as $utilisateur) {?>
-    <p> Nom de l'utilisateur : <?php echo $utilisateur ["nom"]; ?> </p>
-    <p> Adresse mail de l'utilisateur : <?php echo $utilisateur ["email"]; ?> </p>
-   
+
+
     <?php
-    }
-    ?>
-       
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.jade.min.css"
->
+    foreach ($utilisateurs as $utilisateur): ?>
+        <div class="card">
+
+            <p> Nom de l'utilisateur : <?php echo $utilisateur["nom"]; ?> </p>
+            <p> Adresse mail de l'utilisateur : <?php echo $utilisateur["email"]; ?> </p>
+        </div>
+        <?php
+    endforeach; ?>
+    </div>
+
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.jade.min.css">
+    <link rel="stylesheet" href="../../public/style.css">
 </body>
+
 </html>
